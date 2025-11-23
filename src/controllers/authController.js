47ec62b8c694d9a1a6ff1ce9f38dc26a4019ed1c
AlexIA075 <163032623+irlepee@users.noreply.github.com>
2025-11-23 02:02:36 -0700

    FRONTEND CONECTADO - Login funciona

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,6 +3,7 @@ const { validateRegister } = require('../validators/authValidator');
 
 async function register(req, res) {
     try {
+        console.log(req.body);
         const data = validateRegister(req.body);
         const result = await authService.registerUser(req.body);
         res.status(201).json(result);
@@ -12,10 +13,11 @@ async function register(req, res) {
 }
 
 async function login(req, res) {
-    const { username, email, password } = req.body;
+
+    const { identifier, password } = req.body;
 
     try {
-        const result = await authService.loginUser(username, email, password);
+        const result = await authService.loginUser(identifier, password);
         res.status(200).json(result);
     } catch (error) {
         res.status(401).json({ error: error.message });
